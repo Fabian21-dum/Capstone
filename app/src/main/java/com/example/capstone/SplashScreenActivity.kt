c016e064cfe82f3b39fef5e4d18c609770a8a1f2
Fabian21-dum <fabianabin21@gmail.com>
2024-11-18 19:01:24 +0800

    fix splash screen error

--- app/src/main/java/com/example/capstone/SplashScreenActivity.kt
+++ app/src/main/java/com/example/capstone/SplashScreenActivity.kt
@@ -6,12 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
-        installSplashScreen()
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 5000) // 5000 milliseconds = 5 seconds
     }
 }
