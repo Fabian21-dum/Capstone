bfe3140549e17076c2fd729fce9e8bf715361c0f
Fabian21-dum <fabianabin21@gmail.com>
2024-11-18 16:20:09 +0800

    Making Splash Screen

---
+++ app/src/main/java/com/example/capstone/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.capstone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
